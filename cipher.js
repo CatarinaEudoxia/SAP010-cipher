01335ed69eff0e8f79d9b8dd1c470d8e8df8355e
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-05-08 18:11:13 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,5 +1,44 @@
 const cipher = {
-  // ...
+  encode: function (offset, message) {
+    if (typeof offset !== 'number' || typeof message !== 'string') {
+      throw new TypeError();
+    }
+    let encodedMessage = "";
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const encodedCharCode = ((charCode - 65 + offset) % 26) + 65;
+        encodedMessage += String.fromCharCode(encodedCharCode);
+      }
+      else if (charCode >= 97 && charCode <= 122) {
+        const encodedCharCode = ((charCode - 97 + offset) % 26) + 97;
+        encodedMessage += String.fromCharCode(encodedCharCode);
+      } else {
+        encodedMessage += message[i];
+      }
+    }
+    return encodedMessage;
+  },
+  decode: function (offset, message) {
+    if (typeof offset !== 'number' || typeof message !== 'string') {
+      throw new TypeError();
+    }
+    let decodedMessage = "";
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const decodedCharCode = ((charCode - 90 - offset) % 26) + 90;
+        decodedMessage += String.fromCharCode(decodedCharCode);
+      }
+      else if (charCode >= 97 && charCode <= 122) {
+        const decodedCharCode = ((charCode - 122 - offset) % 26) + 122;
+        decodedMessage += String.fromCharCode(decodedCharCode);
+      } else {
+        decodedMessage += message[i];
+      }
+    }
+    return decodedMessage;
+  }
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let offSetValue = 1;
+let activeTab = "tabCripto";
+let lastAction = "encode";
+
+const message = document.querySelector('#textUm');
+const result = document.querySelector('#textDois');
+const msgCod = document.querySelector('#msgCod');
+const msgDecod = document.querySelector('#msgDecod');
+const deslocamento = document.querySelector('#deslocamento');
+
+function applyAction() {
+  const messageValue = message.value;
+  if (lastAction === "encode") {
+    const encrypted = cipher.encode(offSetValue, messageValue);
+    result.value = encrypted;
+    console.log(`Mensagem criptografada: ${result.value}`);
+  } else {
+    const decrypted = cipher.decode(offSetValue, messageValue);
+    result.value = decrypted;
+    console.log(`Mensagem descriptografada: ${result.value}`);
+  }
+}
+
+message.addEventListener('input', applyAction);
+
+msgCod.addEventListener('click', () => {
+  lastAction = "encode";
+  applyAction();
+});
+
+msgDecod.addEventListener('click', () => {
+  lastAction = "decode";
+  applyAction();
+});
+
+deslocamento.addEventListener('change', (event) => {
+  const value = event.target.value;
+  offSetValue = Number(value);
+  applyAction();
+});
+
+const tabCripto = document.querySelector("#tabCripto");
+const tabDecripto = document.querySelector("#tabDecripto");
+
+function toggleTab(event) {
+  tabCripto.classList.remove("active")
+  tabDecripto.classList.remove("active")
+
+  const actualtab = event.target
+  actualtab.classList.add("active")
+
+  const tabId = event.srcElement.id
+  if (activeTab !== tabId) {
+    activeTab = tabId;
+    applyAction();
+  }
+}
+
+tabCripto.addEventListener('click', () => {
+  lastAction = "encode";
+  toggleTab(event);
+});
+
+tabDecripto.addEventListener('click', () => {
+  lastAction = "decode";
+  toggleTab(event);
+});
